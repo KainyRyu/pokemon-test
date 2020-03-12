947bd10b307a628a0dae2ac1fd0c07b956f66c01
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-12 15:50:33 +0000

    getting photo and name

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,24 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import './pokemon.css';
 
 export default function Pokemon() {
-    const [pokemon, setPokemon] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    // const [pokemon, setPokemon] = useState('');
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+
+    function generator() {
+        setIsLoading(!isLoading)
+    }
+
+    function randomNum() {
+        return Math.floor(Math.random() * 50)
+    }
 
     useEffect(() => {
-        fetchPokemon();
+        isLoading && fetchPokemon();
         async function fetchPokemon() {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/25`)
-            const data = await response.json();
-            setPokemon(data)
-
-    }
-    },[]);
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${randomNum()}`)
+            const data = await response.json()
+            setName(data.name);
+            setImage(data.sprites.front_default);
+            setIsLoading(false);
+        }
+    },[isLoading]);
 
     return (
-        <div> 
-
-
-                <h1>{pokemon.name}</h1>
-                {/* <img src={pokemon.sprites.front_default} alt='' /> */}
+        <div className='pokemon'>  
+            <div className='card'>
+                <button className="btn" onClick={generator}>Generator!</button>
+                {
+                    isLoading ? 
+                        <h1>Loading..</h1> : 
+                        <>
+                            <h1>{name}</h1>
+                            <img src={image} alt={name} />
+                        </>
+                }
+            </div>
             
         </div>
     )
