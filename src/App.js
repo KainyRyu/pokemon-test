c5103bb32f2fdacf27e2fcaa1831d70c384395d8
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-18 20:59:48 +0000

    getting the data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Pokemon from './components/Pokemon/Pokemon'
+import Pokemon2 from './components/Pokemon/Pokemon2'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokemon />
+      {/* <Pokemon /> */}
+      <Pokemon2 />
     </div>
   );
 }
--- src/components/Pokemon/Pokemon2.js
+++ src/components/Pokemon/Pokemon2.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './pokemon.css';
 
 export default function Pokemon2() {
-    const [generating, setGenerating] = useState();
-    const [name, setName] = useState();
-    
+    const [isLoading, setIsLoading] = useState(false);
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+
+    function randomNum() {
+        return Math.round(Math.random() * 100);
+    }
     useEffect(() => {
+        isLoading && fetchPokemon();
+        async function fetchPokemon() {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${randomNum()}`)
+            const { name, sprites } = await response.json()
+            setIsLoading(!isLoading);
+            setImage(sprites.front_default)
+            setName(name)
+        }
+    }, [isLoading]);
 
-    }, [])
     return (
-
-        <div>
-            <img src={} alt="" />
-            <h2>{name}</h2>
-            <button onClick={() => }>Click!</button>
+        <div className='card'>
+            <button onClick={() => setIsLoading(!isLoading) }>Click!</button>
+            {isLoading ? 
+                <h1>Loading...</h1> :
+                <div>
+                    <img src={image} alt={name} />
+                    <h2>{name}</h2>
+                </div>
+            }
             
         </div>
     )
