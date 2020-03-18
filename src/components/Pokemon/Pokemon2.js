114c901366f9c4b391d16c62bf54251492c51536
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-18 20:18:49 +0000

    practice hooks from scratch

---
+++ src/components/Pokemon/Pokemon2.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Pokemon2() {
+    const [generating, setGenerating] = useState();
+    const [name, setName] = useState();
+    
+    useEffect(() => {
+
+    }, [])
+    return (
+
+        <div>
+            <img src={} alt="" />
+            <h2>{name}</h2>
+            <button onClick={() => }>Click!</button>
+            
+        </div>
+    )
+}
